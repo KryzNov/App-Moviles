dc970e4faf2809c095435c20d17d37f8c9b225a2
KryzNov <kryzglz02@gmail.com>
2024-09-09 17:11:07 -0600

    Row_Column

--- app/src/main/java/com/example/mya/MainActivity.kt
+++ app/src/main/java/com/example/mya/MainActivity.kt
@@ -5,43 +5,63 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Card
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.ComposableTarget
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.mya.ui.theme.MyATheme
 import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        //enableEdgeToEdge()
         setContent {
 
             Column(
-                modifier = Modifier.fillMaxSize(),
+                modifier = Modifier.fillMaxSize()
+                    .verticalScroll(rememberScrollState()),
                 verticalArrangement = Arrangement.Center,
                 horizontalAlignment = androidx.compose.ui.Alignment.CenterHorizontally
             ){
-                Text(text = "Simple Text")
-                ModifierExample()
-                ModifierExample2()
-                ModifierExample3()
+                CustomText()
+                Picture()
+                Content1()
+                //Text(text = "Simple Text")
+                //ModifierExample()
+                //ModifierExample2()
+                //ModifierExample3()
             }
         }
     }
@@ -55,7 +75,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-@Preview(showBackground = true)
+//@Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     MyATheme {
@@ -63,7 +83,7 @@ fun GreetingPreview() {
     }
 }
 
-@Preview(showBackground = true)
+//@Preview(showBackground = true)
 @Composable
 fun ModifierExample(){
     Column (
@@ -74,7 +94,7 @@ fun ModifierExample(){
     }
 }
 
-@Preview(showBackground = true)
+//@Preview(showBackground = true)
 @Composable
 fun ModifierExample2(){
     Column (
@@ -87,7 +107,7 @@ fun ModifierExample2(){
     }
 }
 
-@Preview(showBackground = true)
+//@Preview(showBackground = true)
 @Composable
 fun ModifierExample3(){
     Column (
@@ -107,7 +127,118 @@ fun ModifierExample3(){
     }
 }
 
+@Preview(showBackground = true)
+@Composable
+fun CustomText(){
+    Column{
+        Text(
+            stringResource(R.string.hello_world_text),
+            color= colorResource(R.color.purple_700),
+            fontSize = 28.sp,
+            fontStyle = FontStyle.Italic,
+            fontWeight = FontWeight.ExtraBold
+        )
+        val gradientColors = listOf(Color.Cyan,Color.Blue)
+        Text(
+            stringResource(R.string.hello_world_text),
+            style = TextStyle(brush = Brush.linearGradient(colors = gradientColors))
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun Picture(){
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color.Black)
+    ) {
+        Image(
+            modifier = Modifier
+                .fillMaxWidth(),
+            painter = painterResource(R.drawable.a) ,
+            contentDescription ="Logo Android",
+            contentScale = ContentScale.Crop
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun Content1(){
+    Card(
+        modifier = Modifier
+            .background(Color.LightGray)
+            .fillMaxWidth()
+            .padding(5.dp)
+    ){
+        Text(text="This is a title",
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .padding(10.dp)
+        )
+        Image(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(height = 200.dp),
+            painter = painterResource(id = R.drawable.a),
+            contentDescription = "Android Logo",
+            contentScale = ContentScale.Crop
+        )
+        Text(stringResource(R.string.text_card),
+            textAlign = TextAlign.Justify,
+            lineHeight = 18.sp,
+            modifier = Modifier
+                .padding(10.dp)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun Content2(){
+    Card(
+        modifier = Modifier
+            .background(Color.LightGray)
+            .fillMaxWidth()
+            .padding(5.dp)
+    ){
+     Row{
+         Image(
+             modifier = Modifier
+                 .width(width = 150.dp)
+                 .height(height = 150.dp),
+             painter = painterResource(id = R.drawable.a),
+             contentDescription = "Android Logo",
+             contentScale = ContentScale.Crop
+         )
+
+         Column {
+             Text(text="This is a title",
+                 fontSize = 16.sp,
+                 fontWeight = FontWeight.Bold,
+                 modifier = Modifier
+                     .padding(10.dp)
+             )
+             Text(stringResource(R.string.text_card),
+                 textAlign = TextAlign.Justify,
+                 maxLines = 7,
+                 lineHeight = 16.sp,
+                 modifier = Modifier
+                     .padding(10.dp)
+             )
+         }
+
+     }
+    }
+}
+
+
 fun clickAction(){
     println("Column Clicked")
 }
+
+
 
