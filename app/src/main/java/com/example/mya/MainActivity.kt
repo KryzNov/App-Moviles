36cb4c690328fd9336de5b39d6f7425870fe1e5b
KryzNov <kryzglz02@gmail.com>
2024-09-19 23:23:05 -0600

    Add interfaz

--- app/src/main/java/com/example/mya/MainActivity.kt
+++ app/src/main/java/com/example/mya/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -21,10 +22,21 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Badge
+import androidx.compose.material3.BadgedBox
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
@@ -36,6 +48,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
@@ -53,6 +66,7 @@ import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+//import com.example.mya.ui.screens.AppContent
 import com.example.mya.ui.screens.HomeScreen
 import com.example.mya.ui.screens.MenuScreen
 
@@ -63,8 +77,9 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         //enableEdgeToEdge()
         setContent {
+            AppContent()
 
-            ComposeMultiScreenApp()
+            //ComposeMultiScreenApp()
            /* Column(
                 modifier = Modifier.fillMaxSize()
                     .verticalScroll(rememberScrollState()),
@@ -79,6 +94,415 @@ class MainActivity : ComponentActivity() {
                 //ModifierExample2()
                 //ModifierExample3()
             }*/
+        }
+    }
+}
+@Composable
+fun AppContent() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(red =15 , green =15 , blue= 16))
+    ) {
+        TopBarMenu()
+        Spacer(modifier = Modifier.height(16.dp))
+        LiveChannelsSection()
+        Spacer(modifier = Modifier.height(16.dp))
+        FollowedCategoriesSection()
+        Spacer(modifier = Modifier.height(16.dp))
+        TextChannels()
+        RecommendedChannelsSection()
+    }
+}
+
+@Preview()
+@Composable
+fun TopBarMenu() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+
+        Icon(
+            painter = painterResource(id = R.drawable.ic_user),
+            contentDescription = "Perfil",
+            tint = colorResource(id =R.color.userColor),
+            modifier = Modifier.size(40.dp) // Ajusta el tamaño si es necesario
+        )
+
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(30.dp)
+        ) {
+            BadgedBox(badge = {
+                Badge { Text("1") }
+            }) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_message),
+                    contentDescription = "Primera notificación",
+                    tint = Color.White,
+                    modifier = Modifier.size(24.dp)
+                )
+            }
+
+
+            BadgedBox(badge = {
+                Badge { Text("2") }
+            }) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_notifications),
+                    contentDescription = "Segunda notificación",
+                    tint = Color.White,
+                    modifier = Modifier.size(24.dp)
+                )
+            }
+
+            Button(onClick = {  },
+                colors = ButtonDefaults.buttonColors(Color(0xFF353535))) {
+                Image(
+                    painterResource(R.drawable.btn_crear),
+                    contentDescription = "Boton crear",
+                    modifier = Modifier
+                        .wrapContentSize()
+                )
+
+            }
+        }
+    }
+}
+
+@Preview()
+@Composable
+fun LiveChannelsSection() {
+    Column(
+        modifier = Modifier
+            .padding(16.dp)
+    ) {
+        Text(
+            text = "Tus canales en vivo",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.White
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painterResource(R.drawable.r),
+                contentDescription = "Imagen de canal",
+                modifier = Modifier
+                    .height(height = 80.dp)
+                    .width(width = 150.dp),
+                contentScale = ContentScale.Crop
+            )
+            Spacer(modifier = Modifier.width(15.dp))
+            Column {
+                Row {
+                    Image(
+                        painterResource(R.drawable.fro),
+                        contentDescription = "Imagen de canal",
+                        modifier = Modifier
+                            .padding(end = 5.dp)
+                            .height(height = 23.dp)
+                            .width(width = 23.dp),
+                        contentScale = ContentScale.Crop
+                    )
+                    Text(text = "RocketLeague", fontWeight = FontWeight.Bold, color = Color.White, fontSize = 17.sp, modifier = Modifier .padding(vertical = 4.dp))
+                }
+
+                Text(text = "Team BDS vs M8 Alpine | Roc....",color = Color.White, fontSize = 16.sp,modifier = Modifier .padding(vertical = 4.dp))
+                Text(text = "Rocket League",color = Color.White, fontSize = 14.sp, modifier = Modifier .padding(vertical = 5.dp))
+                Row {
+                    Text(
+                        text = "Esports",
+                        color = Color.White,fontSize = 12.sp,
+                        modifier = Modifier
+                            .background(Color(0xFF2a292e), shape = RoundedCornerShape(10.dp)) // Fondo gris y esquinas redondeadas
+                            .padding(4.dp)
+                    )
+                    Spacer(modifier = Modifier.width(6.dp))
+                    Text(
+                        text = "English",
+                        color = Color.White,fontSize = 12.sp,
+                        modifier = Modifier
+                            .background(Color(0xFF2a292e), shape = RoundedCornerShape(10.dp)) // Fondo gris y esquinas redondeadas
+                            .padding(4.dp)
+                    )
+                    Spacer(modifier = Modifier.width(6.dp))
+                    Text(
+                        text = "DropsEnabled",
+                        color = Color.White,fontSize = 12.sp,
+                        modifier = Modifier
+                            .background(Color(0xFF2a292e), shape = RoundedCornerShape(10.dp)) // Fondo gris y esquinas redondeadas
+                            .padding(4.dp)
+                    )
+
+                }
+            }
+        }
+    }
+}
+
+@Preview()
+@Composable
+fun FollowedCategoriesSection() {
+    Column(
+        modifier = Modifier
+            .background(Color(red = 15, green = 15, blue = 16))
+            .padding(16.dp)
+    ) {
+        Text(
+            text = "Categorías que sigues",
+            fontSize = 20.sp,
+            color = Color.White,
+            fontWeight = FontWeight.Bold
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+
+        // Lista de categorías con viewers
+        LazyRow(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            items(listOf(
+                CategoryData("Overwatch 2", R.drawable.overwatch2, "14.9 K"),
+                CategoryData("Fortnite", R.drawable.fort, "50.6 K"),
+                CategoryData("Tom Clancy's R6", R.drawable.r6, "50.3 K")
+            )) { category ->
+                CategoryCard(category.title, category.imageRes, category.viewers)
+            }
+        }
+    }
+}
+
+data class CategoryData(val title: String, val imageRes: Int, val viewers: String)
+
+@Composable
+fun CategoryCard(title: String, imageRes: Int, viewers: String) {
+    Card(
+        shape = RectangleShape,
+        modifier = Modifier
+            .wrapContentHeight()
+            .width(130.dp)
+            .padding(vertical = 8.dp)
+            .background(Color(red = 15, green = 15, blue = 16))
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .background(Color(red = 15, green = 15, blue = 16))
+        ) {
+            Image(
+                painter = painterResource(id = imageRes),
+                contentDescription = title,
+                modifier = Modifier.height(150.dp),
+                contentScale = ContentScale.Crop
+            )
+            Text(
+                text = title,
+                fontWeight = FontWeight.Bold,
+                fontSize = 14.sp,
+                color = Color.White,
+                modifier = Modifier
+                    .background(Color(red = 15, green = 15, blue = 16))
+                    .padding(top = 6.dp),
+                textAlign = TextAlign.Left
+            )
+            Row(horizontalArrangement = Arrangement.Start) {
+                Icon(
+                    painter = painterResource(id = R.drawable.viwers),
+                    contentDescription = "viewers",
+                    tint = Color.Red,
+                    modifier = Modifier.size(16.dp).align(Alignment.CenterVertically).padding(top = 4.dp)
+                )
+                Text(
+                    text = viewers,
+                    fontSize = 12.sp,
+                    color = Color.Gray,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier
+                        .align(Alignment.CenterVertically)
+                        .background(Color(red = 15, green = 15, blue = 16))
+                        .padding(top = 4.dp)
+                )
+            }
+        }
+    }
+}
+
+
+
+
+
+@Preview
+@Composable
+fun TextChannels(){
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+    ) {
+        Text(
+            text = "Canales recomendados para ti",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.White
+        )
+    }
+}
+@Preview
+@Composable
+fun RecommendedChannelsSection() {
+    Box(modifier = Modifier.fillMaxSize()
+        .padding(horizontal = 16.dp)) {
+
+        // Contenido desplazable
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(bottom = 20.dp)
+                .verticalScroll(rememberScrollState()) // Habilitar el desplazamiento vertical
+        ) {
+            Spacer(modifier = Modifier.height(3.dp))
+
+
+            val channels = listOf(
+                ChannelData("Aspen", R.drawable.v1, R.drawable.p1, "TOP 50 SUPPORT WO...", "Overwatch 2", listOf( "English")),
+                ChannelData("Jay3", R.drawable.v2, R.drawable.p2, "BONUS BITS START T...", "Overwatch 2", listOf("FPS", "Shooter", "Multiplayer")),
+                ChannelData("RocketLeague", R.drawable.r, R.drawable.fro, "Team BDS vs M8 Alpine | Roc....", "Rocket League", listOf("Esports", "English", "DropsEnabled")),
+
+                )
+
+            channels.forEach { channel ->
+                LiveChannelCard(channel)
+                Spacer(modifier = Modifier.height(16.dp))
+            }
+        }
+
+        // Menú fijo en la parte inferior
+        BottomMenu(modifier = Modifier.align(Alignment.BottomCenter))
+    }
+}
+
+data class ChannelData(
+    val channelName: String,
+    val channelImage: Int,
+    val logoImage: Int,
+    val title: String,
+    val game: String,
+    val tags: List<String>
+)
+
+@Composable
+fun LiveChannelCard(channel: ChannelData) {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Image(
+            painter = painterResource(channel.channelImage),
+            contentDescription = "Imagen de canal",
+            modifier = Modifier
+                .height(80.dp)
+                .width(150.dp),
+            contentScale = ContentScale.Crop
+        )
+        Spacer(modifier = Modifier.width(15.dp))
+        Column {
+            Row {
+                Image(
+                    painter = painterResource(channel.logoImage),
+                    contentDescription = "Imagen de canal",
+                    modifier = Modifier
+                        .padding(end = 5.dp)
+                        .height(23.dp)
+                        .width(23.dp),
+                    contentScale = ContentScale.Crop
+                )
+                Text(
+                    text = channel.channelName,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.White,
+                    fontSize = 17.sp,
+                    modifier = Modifier.padding(vertical = 4.dp)
+                )
+            }
+            Text(text = channel.title, color = Color.White, fontSize = 16.sp, modifier = Modifier.padding(vertical = 4.dp))
+            Text(text = channel.game, color = Color.White, fontSize = 14.sp, modifier = Modifier.padding(vertical = 5.dp))
+            Row {
+                channel.tags.forEach { tag ->
+                    Text(
+                        text = tag,
+                        color = Color.White,
+                        fontSize = 12.sp,
+                        modifier = Modifier
+                            .background(Color(0xFF2a292e), shape = RoundedCornerShape(10.dp))
+                            .padding(4.dp)
+                    )
+                    Spacer(modifier = Modifier.width(6.dp))
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun BottomMenu(modifier: Modifier = Modifier) {
+    Row(
+        modifier = modifier
+            .background(Color(red =15 , green =15 , blue= 16))
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp, vertical = 5.dp),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Icon(
+                imageVector = Icons.Default.Favorite, // Cambia por tu ícono morado
+                contentDescription = "Siguiendo",
+                tint = Color(0xFF9B59B6) // Morado
+            )
+            Text(text = "Siguiendo", color = Color(0xFF9B59B6), fontSize = 12.sp)
+        }
+
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Icon(
+                painter = painterResource(id = R.drawable.baseline_queue_play_next_24), // Cambia por tu ícono de pantalla
+                contentDescription = "Feed",
+                tint = Color.White
+            )
+            Text(text = "Feed", color = Color.White, fontSize = 12.sp)
+        }
+
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Icon(
+                painter = painterResource(id = R.drawable.baseline_explore_24),
+                contentDescription = "Descubrir",
+                tint = Color.White
+            )
+            Text(text = "Descubrir", color = Color.White, fontSize = 12.sp)
+        }
+
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Icon(
+                painter = painterResource(id = R.drawable.baseline_content_copy_24),
+                contentDescription = "Explorar",
+                tint = Color.White
+            )
+            Text(text = "Explorar", color = Color.White, fontSize = 12.sp)
+        }
+
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Icon(
+                imageVector = Icons.Default.Search, // Cambia por tu ícono de lupa
+                contentDescription = "Buscar",
+                tint = Color.White
+            )
+            Text(text = "Buscar", color = Color.White, fontSize = 12.sp)
         }
     }
 }
@@ -300,7 +724,7 @@ fun BoxExample2(){
 fun clickAction(){
     println("Column Clicked")
 }
- */
+
 
 @Composable
 fun ComposeMultiScreenApp(){
@@ -317,7 +741,7 @@ fun SetupNavGraph(navController: NavHostController){
         composable("home"){ HomeScreen(navController) }
     }
 }
-
+*/
 
 
 
