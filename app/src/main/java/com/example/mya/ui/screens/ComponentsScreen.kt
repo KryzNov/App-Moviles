4d45033110d31361e6aadc66b7fb2d3e7e4f7a8f
KryzNov <kryzglz02@gmail.com>
2024-09-28 16:31:24 -0600

    Added navigation drawer and buttons

--- app/src/main/java/com/example/mya/ui/screens/ComponentsScreen.kt
+++ app/src/main/java/com/example/mya/ui/screens/ComponentsScreen.kt
@@ -1,14 +1,34 @@
 package com.example.mya.ui.screens
 
+
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.AssistChip
+import androidx.compose.material3.AssistChipDefaults
 import androidx.compose.material3.Button
 import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.FilledTonalButton
+import androidx.compose.material3.FilterChip
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconToggleButton
+import androidx.compose.material3.LargeFloatingActionButton
 import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.NavigationDrawerItem
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.SmallFloatingActionButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -16,7 +36,9 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import kotlinx.coroutines.launch
@@ -33,12 +55,12 @@ fun ComponentsScreen(navController: NavController){
             ModalDrawerSheet {
                 Text("Menu", modifier = Modifier .padding(16.dp))
                 HorizontalDivider()
-                //Content1
+                //Buttons
                 NavigationDrawerItem(
-                    label = { Text("Content1") },
+                    label = { Text("Buttons") },
                     selected = false,
                     onClick = {
-                        component ="Content1"
+                        component ="Buttons"
                         scope.launch {
                             drawerState.apply {
                                 close()
@@ -46,12 +68,25 @@ fun ComponentsScreen(navController: NavController){
                         }
                     }
                 )
-                //Content2
+                //FloatingButtons
                 NavigationDrawerItem(
-                    label = { Text("Content2") },
+                    label = { Text("FloatingButtons") },
                     selected = false,
                     onClick = {
-                        component ="Content2"
+                        component ="FloatingButtons"
+                        scope.launch {
+                            drawerState.apply {
+                                close()
+                            }
+                        }
+                    }
+                )
+                //Chips
+                NavigationDrawerItem(
+                    label = { Text("Chips") },
+                    selected = false,
+                    onClick = {
+                        component ="Chips"
                         scope.launch {
                             drawerState.apply {
                                 close()
@@ -65,11 +100,14 @@ fun ComponentsScreen(navController: NavController){
         //Screen Content
         Column {
             when(component){
-                "Content1" -> {
-                    Content1()
+                "Buttons" -> {
+                    Buttons()
                 }
-                "Content2" -> {
-                    Content2()
+                "FloatingButtons" -> {
+                    FloatingButtons()
+                }
+                "Chips" -> {
+                    Chips()
                 }
             }
         }
@@ -86,4 +124,94 @@ fun Content1(){
 @Composable
 fun Content2(){
     Text(text = "Content 2")
+}
+
+
+@Composable
+fun Buttons(){
+    Column (
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly,
+        modifier = Modifier
+            .fillMaxSize()
+    ){
+        Button(onClick = {}){
+            Text("Filled")
+        }
+        FilledTonalButton(onClick = {}){
+            Text("Tonal")
+        }
+        OutlinedButton (onClick = {}){
+            Text("Outline")
+        }
+        ElevatedButton (onClick = {}){
+            Text("Elevated")
+        }
+        TextButton (onClick = {}){
+            Text("Text")
+        }
+    }
+}
+
+
+@Composable
+fun FloatingButtons(){
+    Column (
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly,
+        modifier = Modifier
+            .fillMaxSize()
+    ){
+        FloatingActionButton(onClick = {}) {
+            Icon(Icons.Filled.Add,"")
+        }
+        SmallFloatingActionButton (onClick = {}) {
+            Icon(Icons.Filled.Add,"")
+        }
+        LargeFloatingActionButton(onClick = {}) {
+            Icon(Icons.Filled.Add,"")
+        }
+        ExtendedFloatingActionButton (
+            onClick = {},
+            icon = {Icon(Icons.Filled.Add, "")},
+            text = { Text(text = "Extended FAB")}
+        )
+
+    }
+}
+
+@Preview
+@Composable
+fun Chips() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly,
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        AssistChip(
+            onClick = {},
+            label = { Text("Assist Chip")},
+            leadingIcon = {
+                Icon(Icons.Filled.AccountBox,"",
+                    Modifier.size(AssistChipDefaults.IconSize))
+            }
+        )
+        var selected by remember { mutableStateOf(false) }
+        FilterChip(
+            selected = selected,
+            onClick = {selected=!selected},
+            label = { Text("Filter Chip")},
+            leadingIcon = if(selected){
+                {
+                    Icon(
+                        Icons.Filled.AccountBox, "",
+                        Modifier.size(AssistChipDefaults.IconSize)
+                    )
+                }
+            }else {
+                null
+            }
+        )
+    }
 }
