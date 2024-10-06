d52ec7a90c04d972803dadbd4450898397f8a102
KryzNov <kryzglz02@gmail.com>
2024-10-06 14:25:59 -0600

    Added bars

--- app/src/main/java/com/example/mya/ui/screens/ComponentsScreen.kt
+++ app/src/main/java/com/example/mya/ui/screens/ComponentsScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -16,6 +17,10 @@ import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.material.icons.filled.Add
@@ -27,6 +32,11 @@ import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.material.icons.filled.Warning
+import androidx.compose.material.icons.outlined.Favorite
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Notifications
+import androidx.compose.material.icons.outlined.Person
+import androidx.compose.material.icons.outlined.Search
 import androidx.compose.material3.AssistChip
 import androidx.compose.material3.AssistChipDefaults
 import androidx.compose.material3.Badge
@@ -81,12 +91,16 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Popup
 import androidx.navigation.NavController
+import com.example.mya.R
+import com.example.mya.data.model.PostModel
 import kotlinx.coroutines.launch
 import java.text.SimpleDateFormat
 import java.util.Calendar
@@ -803,7 +817,7 @@ fun AlertDialogs() {
         }
     }
 }
-
+@Preview
 @Composable
 fun Bars(){
     Box(modifier = Modifier
@@ -825,6 +839,103 @@ fun Bars(){
                 fontSize = 20.sp
             )
             Icon(Icons.Filled.Settings, contentDescription = "", tint = Color.White)
+        }
+        var post = arrayOf(
+            PostModel(1,"Title 1", "Text1"),
+            PostModel(2,"Title 2", "Text2"),
+            PostModel(3,"Title 3", "Text3"),
+            PostModel(4,"Title 4", "Text4")
+        )
+        Column(
+            modifier = Modifier
+                .align(Alignment.TopCenter)
+                .padding(10.dp,90.dp,10.dp,50.dp)
+                .fillMaxSize()
+        ){
+            Posts(post)
+        }
+
+
+        Row (
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .fillMaxWidth()
+                .height(65.dp)
+                .background(Color.Black)
+                .padding(2.dp,5.dp),
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ){
+            Column {
+                IconButton(onClick = {}, Modifier.size(30.dp)) {
+                    Icon(Icons.Outlined.Home,
+                        contentDescription = "",
+                        tint = Color.White,
+                        modifier = Modifier.fillMaxSize()
+                    )
+                }
+                Text(text = "Home", color=Color.White)
+            }
+            Column {
+                IconButton(onClick = {}, Modifier.size(30.dp)) {
+                    Icon(Icons.Outlined.Search,
+                        contentDescription = "",
+                        tint = Color.White,
+                        modifier = Modifier.fillMaxSize()
+                    )
+                }
+                Text(text = "Search", color=Color.White)
+            }
+            Column {
+                IconButton(onClick = {}, Modifier.size(30.dp)) {
+                    Icon(Icons.Outlined.Favorite,
+                        contentDescription = "",
+                        tint = Color.White,
+                        modifier = Modifier.fillMaxSize()
+                    )
+                }
+                Text(text = "Favorite", color=Color.White)
+            }
+            Column {
+                IconButton(onClick = {}, Modifier.size(30.dp)) {
+                    Icon(Icons.Outlined.Notifications,
+                        contentDescription = "",
+                        tint = Color.White,
+                        modifier = Modifier.fillMaxSize()
+                    )
+                }
+                Text(text = "Alerts", color=Color.White)
+            }
+            Column {
+                IconButton(onClick = {}, Modifier.size(30.dp)) {
+                    Icon(Icons.Outlined.Person,
+                        contentDescription = "",
+                        tint = Color.White,
+                        modifier = Modifier.fillMaxSize()
+                    )
+                }
+                Text(text = "Profile", color=Color.White)
+            }
+
+
+        }
+    }
+}
+
+@Composable
+fun Posts(arrayPosts:Array<PostModel>){
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+
+    ) {
+        items(arrayPosts){ post->
+            Text(
+                text = post.text,
+                color = Color.White,
+                fontSize = 16.sp
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            HorizontalDivider(thickness = 2.dp)
         }
     }
 }
